0216d476e94c8d462acf5b1bbd5327b7d939c54a
engineeredwin <gtitechnet@gmail.com>
2023-03-02 17:04:39 -0600

    added vagrant folder

---
+++ vagrant/Automated_provisioning/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"222337fd20d6bd8715c74ce74b801212da6811a890ac7c510c9ca7d32817c652","vagrant_version":"2.3.1"}
